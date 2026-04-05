1600ffe066a32745aad7029a9f94e45d60ad67b7
VampireFlower <monstercatspedup1@gmail.com>
2026-04-05 00:07:52 -0400

    add some native code

---
+++ worlds/mgtt/patch/mgtt.h
@@ -0,0 +1,68 @@
+// // clang -Os -target powerpc-eabi -mcpu=750 -mbig-endian -ffreestanding -nostdlib -S file.c  -mllvm -ppc-sdata-threshold=0 -fno_pic
+
+#ifndef MGTT_H
+#define MGTT_H
+
+typedef float mtx[3][4];
+typedef float mtx44[4][4];
+
+typedef void * pTextBlock;
+
+#define C_MTXOrtho ((void (*) \
+(                             \
+    mtx44,                    \
+    float top,                \
+    float bottom,             \
+    float left,               \
+    float right,              \
+    float near,               \
+    float far                 \
+))0x8003e684)
+
+#define PSMTXIdentity       ((void (*)(mtx))                              0x8003c354)
+#define GXSetTexCoordGen2   ((void (*)(int,int,int,int))                  0x80068814)
+#define GXSetNumTexGens     ((void (*)(int))                              0x80068a38)
+#define GXSetCullMode       ((void (*)(int))                              0x8006982c)
+#define GXSetNumChans       ((void (*)(int))                              0x8006afc4)
+#define GXSetChanCtrl       ((void (*)(int, int, int, int, int, int, int))0x8006afe8)
+#define GXSetTevOrder       ((void (*)(int, int, int, int))               0x8006dba0)
+#define GXSetNumTevStages   ((void (*)(int))                              0x8006dd18)
+#define GXSetBlendMode      ((void (*)(int, int, int, int))               0x8006e154)
+#define GXSetZMode          ((void (*)(int, int, int))                    0x8006e23c)
+#define GXSetProjection     ((void (*)(mtx44,int type))                   0x8006fcc0)
+#define GXLoadPosMtxImm     ((void (*)(mtx, int))                         0x8006fe10)
+
+
+
+
+
+
+
+
+#define TextBlockCreate     ((pTextBlock (*)(int, int, int, int, int, int))      0x80024e04)
+#define TextBlockConfigure  ((void (*)(pTextBlock, int strid, int x, int y, int))0x80024cfc)
+#define TextBlockDraw       ((void (*)(int subsystem, mtx, int color, int))      0x80027ecc)
+#define TextBlockDelete     ((void (*)(pTextBlock))                              0x80024c98)
+
+#define add_to_render_queue ((void (*)(float depth, void* drawer, int arg, int priority))0x8001e924)
+
+
+
+float Float(int bits)
+{
+    float out;
+    int tmp;
+
+    __asm__ volatile(
+        "stw %1, 0(%2)\n"
+        "lfs %0, 0(%2)\n"
+        : "=f"(out)
+        : "r"(bits), "r"(&tmp)
+    );
+
+    return out;
+}
+
+
+
+#endif
---
+++ worlds/mgtt/patch/text.c
@@ -0,0 +1,54 @@
+// clang -Os -target powerpc-eabi -mcpu=750 -mbig-endian -ffreestanding -nostdlib -S file.c
+#include "mgtt.h"
+
+
+
+// lvalue that lives at 0x800017B0
+#define mytext (*(int**)0x800017B0)
+
+
+// inject to 80018f4c
+void Text(void)
+{
+
+    mytext = TextBlockCreate(368,192,320,192,1,3);
+    TextBlockConfigure(mytext, 0, 8, 28, 13);
+
+
+    add_to_render_queue(11.0, TextDraw, mytext, 20);
+
+}
+
+
+void TextDraw(pTextBlock block) {
+    
+    mtx44 projection;
+    mtx identity;
+
+    C_MTXOrtho(
+        projection,0,480,0,640,0,-1000
+    );
+
+    GXSetProjection(projection, 1);
+
+    PSMTXIdentity(identity);
+    GXSetBlendMode(1,4,5,15);
+
+    // All vertex positions will now be multiplied by this matrix before projection.
+    GXLoadPosMtxImm(identity, 0);
+    GXSetNumTevStages(1);
+    GXSetTevOrder(0,0,0,4);
+    GXSetChanCtrl(4,0,0,1,0,2,2);
+    GXSetCullMode(0);
+    GXSetZMode(0,7,0);
+    GXSetNumTexGens(1);
+    GXSetNumChans(0);
+    GXSetTexCoordGen2(0,1,4,60);
+    
+    TextBlockDraw(*((int*)block + 0x95), identity, 0xffffffff, 1);
+
+    // if it isnt deleted, the game automatically picks up on the tb and draws it?!
+    // means we can't use effects like typewriter. investigate later
+    TextBlockDelete(block);
+    free(block);
+    }
